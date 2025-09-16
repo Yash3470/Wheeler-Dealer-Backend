4feee111db5e79e5df9dc6f3b33607276d92babc
Yash Chavan <yashchavan933@gmail.com>
2025-09-16 22:06:43 +0530

    Update carRoutes.js

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { isAdmin, requireLogin } = require('../middleware/authMiddleware');
-const {upload,createCar,getAllCar, getCarById,deleteCar,relatedCar,updatecar,braintreeTokenController,brainTreePaymentController} = require('../controllers/carController');
+const {upload,createCar,getAllCar, getCarById,deleteCar,relatedCar,updateCar,braintreeTokenController,brainTreePaymentController} = require('../controllers/carController');
 const formidable = require('express-formidable')
 
 const router = express.Router()
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get('/getAll-car',getAllCar);
 router.get('/getCarById-car/:slug',getCarById);
 router.post('/create-car',requireLogin,isAdmin,upload.array('productPictures',50),createCar);
-router.put('/update-car/:pid',requireLogin,isAdmin,formidable(),updatecar);
+router.put('/update-car/:pid',requireLogin,isAdmin,formidable(),updateCar);
 router.delete('/delete-car/:pid',requireLogin,isAdmin,deleteCar);
 router.get('/related-car/:cid/:bid',relatedCar)
 router.get("/braintree/token", braintreeTokenController);
